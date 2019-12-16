70029ea4d25a7e8fc840d779feeaeecf420ec098
sunny <17765293970@163.com>
2019-12-16 22:11:03 +0800

    fix: add a new leetcode

--- packages/leetcode/__test__/index.spec.ts
+++ packages/leetcode/__test__/index.spec.ts
@@ -1,4 +1,4 @@
-import { reverseWords, countBinarySubstrings } from '../index'
+import { reverseWords, countBinarySubstrings, letterCombinations } from '../index'
 
 test('reverseWords: Let\'s take LeetCode contest => s\'teL ekat edoCteeL tsetnoc', () => {
   expect(reverseWords('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc')
@@ -6,4 +6,10 @@ test('reverseWords: Let\'s take LeetCode contest => s\'teL ekat edoCteeL tsetnoc
 
 test('countBinarySubstrings: 00110011 => ["0011", "01", "1100", "10", "0011", "01"]', () => {
   expect(countBinarySubstrings('00110011')).toEqual(['0011', '01', '1100', '10', '0011', '01'])
+})
+
+describe('letterCombinations', () => {
+  it('letterCombinations: 23 => ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]', () => {
+    expect(letterCombinations('23')).toEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
+  })
 })
--- packages/leetcode/index.ts
+++ packages/leetcode/index.ts
@@ -27,7 +27,7 @@ export const countBinarySubstrings = (s: string) => {
     }
     return false
   }
-  for (let i =0, len = s.length; i < len; i++) {
+  for (let i = 0, len = s.length; i < len; i++) {
     const legacyStr = s.slice(i)
     const matched = match(legacyStr)
     if (matched) {
@@ -35,4 +35,31 @@ export const countBinarySubstrings = (s: string) => {
     }
   }
   return arr // arr.length
+}
+
+/**
+ * 17. 电话号码的字母组合
+ * @param {string} digits
+ * @return {string[]}
+ */
+export const letterCombinations = (digits: string) => {
+  const map = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+  const num = digits.split('')
+  const code: string[] = num.map(n => {
+    return map[n]
+  })
+  const comb = (arr) => {
+    const temp = []
+    for (let i = 0, il = arr[0].length; i < il; i++) {
+      for (let j = 0, jl = arr[1].length; j < jl; j++) {
+        temp.push(arr[0][i] + arr[1][j])
+      }
+    }
+    arr.splice(0, 2, temp)
+    if (arr.length > 1) {
+      comb(arr)
+    }
+    return arr[0]
+  }
+  return comb(code)
 }
