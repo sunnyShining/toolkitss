dadb85cdd6a4324c8a1d7ec1e67621f624e07229
sunshine <17765293970@163.com>
2020-04-19 18:25:02 +0800

    feat: useState换成useReducer写法

--- packages/custom-hook/set-state.ts
+++ packages/custom-hook/set-state.ts
@@ -1,12 +1,27 @@
-import React from 'react'
+import { useReducer, useRef, useEffect, useCallback  } from 'react'
 import { toRawType, invariant, noop } from '@toolkitss/helper'
-import { useForceUpdate } from './force-update'
 
-const { useRef, useEffect, useCallback } = React
-
+function setStateReducer<S>(
+  state: S,
+  action: {
+    type: 'update'
+    nextState: Partial<S>
+  }
+) {
+  switch (action.type) {
+    case 'update':
+      return {
+        ...state,
+        ...action.nextState
+      }
+    default:
+      return state
+  }
+}
+// can use useReducer do it
 export function useSetState<S = object>(
   initSate: S,
-  initCallback?: Function
+  initCallback?: (state: S) => any
 ): [
     S,
     (
@@ -16,34 +31,75 @@ export function useSetState<S = object>(
   ] {
   // @ts-ignore
   invariant(toRawType(initSate) === 'Object', 'initState should be a object')
-  const state = useRef<S>(initSate) // 存放state
+  const [state, dispatch] = useReducer<(state: S, action: {type: 'update'; nextState: Partial<S>;}) => S>(setStateReducer, initSate)
   const updated = useRef<Function>(initCallback || noop)
-  const [, forceUpdate] = useForceUpdate() // 强制更新
   /** make sure update */
   const setState = useCallback((
     nextState: Partial<S> | ((prevState: S) => Partial<S>),
-    callback?: Function
+    callback?: (state: S) => any
   ) => {
     // @ts-ignore
     invariant(
       toRawType(nextState) === 'Object' || typeof nextState === 'function',
       'update state should be object or function'
     )
-    const updateState =
-      typeof nextState === 'function' ? nextState(state.current) : nextState
-    Object.assign(state.current || {}, updateState) // 合并值
-    forceUpdate() // 强制更新props
+    const updateState = typeof nextState === 'function' ? nextState(state) : nextState
+    dispatch({
+      type: 'update',
+      nextState: updateState
+    })
     if (callback) { // 存储callback
       updated.current = callback
     }
   }, [])
   useEffect(() => {
     if (updated.current !== noop && typeof updated.current === 'function') {
-      updated.current(state.current)
+      updated.current(state)
     }
     return noop
   }, [updated.current])
-  return [state.current, setState]
+  return [state, setState]
 }
 
-
+// can use useReducer do it
+// export function useSetState<S = object>(
+//   initSate: S,
+//   initCallback?: Function
+// ): [
+//     S,
+//     (
+//       nextSate: Partial<S> | ((prevState: S) => Partial<S>),
+//       callback?: Function
+//     ) => void
+//   ] {
+//   // @ts-ignore
+//   invariant(toRawType(initSate) === 'Object', 'initState should be a object')
+//   const state = useRef<S>(initSate) // 存放state
+//   const updated = useRef<Function>(initCallback || noop)
+//   const [, forceUpdate] = useForceUpdate() // 强制更新
+//   /** make sure update */
+//   const setState = useCallback((
+//     nextState: Partial<S> | ((prevState: S) => Partial<S>),
+//     callback?: Function
+//   ) => {
+//     // @ts-ignore
+//     invariant(
+//       toRawType(nextState) === 'Object' || typeof nextState === 'function',
+//       'update state should be object or function'
+//     )
+//     const updateState =
+//       typeof nextState === 'function' ? nextState(state.current) : nextState
+//     Object.assign(state.current || {}, updateState) // 合并值
+//     forceUpdate() // 强制更新props
+//     if (callback) { // 存储callback
+//       updated.current = callback
+//     }
+//   }, [])
+//   useEffect(() => {
+//     if (updated.current !== noop && typeof updated.current === 'function') {
+//       updated.current(state.current)
+//     }
+//     return noop
+//   }, [updated.current])
+//   return [state.current, setState]
+// }
