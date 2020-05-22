a267015e00104ea2eddf7b995688c2800f352245
sunshine <17765293970@163.com>
2020-05-22 09:40:10 +0800

    fix: 减少一次执行

--- packages/custom-hook/set-state.ts
+++ packages/custom-hook/set-state.ts
@@ -23,8 +23,9 @@ const logger = (reducer) => {
     return (state, action) => {
       console.log('%cPrevious State:', 'color: #9E9E9E; font-weight: 700;', state)
       console.log('%cAction:', 'color: #00A7F7; font-weight: 700;', action)
-      console.log('%cNext State:', 'color: #47B04B; font-weight: 700;', reducer(state,action))
-      return reducer(state,action)
+      const result = reducer(state,action)
+      console.log('%cNext State:', 'color: #47B04B; font-weight: 700;', result)
+      return result
     }
   }
   return reducer
